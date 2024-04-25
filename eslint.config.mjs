72e4948ac381fd87f2a7ab263666cf5d20a4cb98
flickleafy <8204229+flickleafy@users.noreply.github.com>
2024-04-25 19:16:04 -0400

    eslint configs

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-anonymous-default-export */
+import globals from 'globals';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  ...compat.extends('airbnb'),
+  {
+    rules: {
+      'no-console': 'warn', // Warns about console.log usage
+      'no-unused-vars': 'warn', // Warns about unused variables
+      indent: ['warn', 2], // Enforces a 2-space indentation
+      quotes: ['warn', 'single'], // Enforces single quotes
+      semi: ['warn', 'always'], // Enforces semicolons at the end of statements
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
+  },
+];
