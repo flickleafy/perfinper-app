95b3e4167cc53b4549c4e38fe370a6f3d1c7efbe
flickleafy <8204229+flickleafy@users.noreply.github.com>
2024-05-12 19:37:47 -0400

    component renaming

---
+++ src/components/TransactionsListToolBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, AppBar, Toolbar } from '@mui/material';
+import SearchBar from './SearchBar.js';
+import StatusBar from './StatusBar.js';
+import PeriodSelector from './PeriodSelector.js';
+
+export function TransactionsListToolBar({
+  periodSelected,
+  handleDataChangePeriodSelector,
+  fullTransactionsList,
+  handleDataChangeSearchBar,
+  transactionsPrintList,
+}) {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+        <AppBar
+          position='static'
+          color='primary'>
+          <Toolbar>
+            <PeriodSelector
+              currentPeriod={periodSelected}
+              onDataChange={handleDataChangePeriodSelector}
+            />
+            <SearchBar
+              array={fullTransactionsList}
+              onDataChange={handleDataChangeSearchBar}
+            />
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box marginTop={2}>
+        <StatusBar array={transactionsPrintList} />
+      </Box>
+    </Box>
+  );
+}
+
+TransactionsListToolBar.propTypes = {
+  periodSelected: PropTypes.string,
+  handleDataChangePeriodSelector: PropTypes.func,
+  fullTransactionsList: PropTypes.array,
+  handleDataChangeSearchBar: PropTypes.func,
+  transactionsPrintList: PropTypes.array,
+};
