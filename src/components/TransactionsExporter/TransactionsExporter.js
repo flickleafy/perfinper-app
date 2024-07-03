681954c05786b17b3e07c6c72b78ccb970896fd2
flickleafy <8204229+flickleafy@users.noreply.github.com>
2024-07-02 23:29:08 -0400

    transaction export update

--- src/components/TransactionsExporter/TransactionsExporter.js
+++ src/components/TransactionsExporter/TransactionsExporter.js
@@ -2,98 +2,95 @@ import React, { useState } from 'react';
 import {
   Grid,
   Box,
-  FormControl,
-  InputLabel,
   Button,
   Typography,
+  TextField,
+  Snackbar,
 } from '@mui/material';
 import { exportTransactions } from '../../services/exportService.js';
 
 const TransactionsExporter = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [fileName, setFileName] = useState('');
+  const [fileDownloadUrl, setFileDownloadUrl] = useState(null);
+  const [error, setError] = useState('');
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+  const handleFileNameChange = (event) => {
+    setFileName(event.target.value);
   };
 
   const handleExport = async () => {
-    if (!selectedFile) {
-      return alert('Please select a file to save the export');
+    if (!fileName) {
+      setError('Please enter a file name to save the export');
+      return;
     }
-    console.log('handle', selectedFile);
-    const reader = new FileReader();
-    reader.readAsText(selectedFile);
-    reader.onload = async (e) => {
-      let data = null;
-      // Get file type from selectedFile
-      const fileType = selectedFile.type;
+    setError(''); // Reset error message before the operation
+    try {
+      const data = await exportTransactions(); // Fetch data from the API
+      const isValidJSON = JSON.parse(JSON.stringify(data)); // Attempt to validate JSON data
+      const blob = new Blob([JSON.stringify(data, null, 2)], {
+        type: 'application/json',
+      });
+      if (fileDownloadUrl) window.URL.revokeObjectURL(fileDownloadUrl); // Clean up the previous URL
+      const downloadUrl = window.URL.createObjectURL(blob);
+      setFileDownloadUrl(downloadUrl);
+    } catch (error) {
+      setError(`Failed to export data. Error: ${error.message}`);
+      setFileDownloadUrl(null);
+    }
+  };
 
-      try {
-        if (fileType.includes('json')) {
-          // Check if file type includes "json"
-          data = JSON.parse(e.target.result);
-        } else {
-          return alert(
-            'Unsupported file format. Please select a JSON or CSV file.'
-          );
-        }
-      } catch (error) {
-        console.error('Error parsing file:', error);
-        alert('Error writting file. Please try again.');
-        return;
-      }
-      await exportTransactions(data);
-    };
-    reader.onerror = (error) =>
-      alert('Error writting file', JSON.stringify(error, null, 4));
+  const handleCloseSnackbar = () => {
+    setError('');
   };
 
   return (
-    <Box>
+    <Box
+      paddingLeft={8}
+      paddingRight={8}
+      paddingBottom={8}>
       <Grid
         container
         spacing={2}>
         <Grid
           item
           xs={6}>
-          <FormControl fullWidth>
-            <InputLabel id='importer-select-label'>Importer</InputLabel>
-          </FormControl>
-          {selectedFile && (
-            <Typography
-              variant='body2'
-              sx={{ mt: 2 }}>
-              File: {selectedFile.name}
-            </Typography>
-          )}
+          <TextField
+            fullWidth
+            label='Enter file name'
+            value={fileName}
+            onChange={handleFileNameChange}
+            helperText='File will be saved as JSON'
+          />
         </Grid>
         <Grid
           item
           xs={6}>
-          <input
-            type='file'
-            accept='.json'
-            hidden
-            id='file-input'
-            onChange={handleFileChange}
-          />
-          <label htmlFor='file-input'>
-            <Button
-              variant='contained'
-              component='span'
-              sx={{ marginRight: 1 }}>
-              Selecionar arquivo
-            </Button>
-          </label>
           <Button
             variant='contained'
-            color='primary'
+            component='span'
             onClick={handleExport}
-            disabled={!selectedFile}>
+            disabled={!fileName}>
             Exportar
           </Button>
+          {fileDownloadUrl && (
+            <Typography
+              variant='body2'
+              sx={{ mt: 2 }}>
+              <a
+                href={fileDownloadUrl}
+                download={`${fileName}.json`}>
+                Click here to download the file
+              </a>
+            </Typography>
+          )}
         </Grid>
       </Grid>
+      <Snackbar
+        open={!!error}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        message={error}
+      />
     </Box>
   );
 };
